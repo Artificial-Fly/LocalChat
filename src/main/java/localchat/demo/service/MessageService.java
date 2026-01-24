13954d0e37f38d90db08a85d59cbe7a0d3b317e3
Lazy Fruit <117809440+Artificial-Fly@users.noreply.github.com>
2026-01-24 20:35:17 +0900

    add message service log

--- src/main/java/localchat/demo/service/MessageService.java
+++ src/main/java/localchat/demo/service/MessageService.java
@@ -6,6 +6,8 @@ import localchat.demo.entity.User;
 import localchat.demo.exception.EmptyMessageException;
 import localchat.demo.exception.UserNotFoundException;
 import localchat.demo.repository.MessageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -17,38 +19,54 @@ public class MessageService {
     private int recentMessageDays = 7;
     private int oldMessageDays = 30;
 
+    private static Logger log = LoggerFactory.getLogger(MessageService.class);
+
     public MessageService(MessageRepository messageRepository) {
         this.messageRepository = messageRepository;
     }
 
     //save message
     public Message saveMessage(String content, User user){
+        log.debug("Saving message from user {}", user.getNickname());
         if (content==null||content.trim().isEmpty()){
+            log.debug("EmptyMessageException happened");
             throw new EmptyMessageException("Message is empty");
         }
         if(user==null){
+            log.debug("UserNotFoundException happened");
             throw new UserNotFoundException("User cannot be empty");
         }
         Message message = new Message();
         message.setContent(content);
         message.setSenderNickname(user.getNickname());
         message.setCreatedAt(LocalDateTime.now());
-        return messageRepository.save(message);
+        Message savedMessage = messageRepository.save(message);
+        log.info("Message saved (id={}, nickname={})", savedMessage.getId(), savedMessage.getSenderNickname());
+        return savedMessage;
     }
     //get recent (7d)
     public List<Message> getRecentMessages(){
+        log.debug("Getting recent ({}) messages", recentMessageDays);
         LocalDateTime timeAgo = LocalDateTime.now().minusDays(recentMessageDays);
-        return messageRepository.findMessageSince(timeAgo);
+        List<Message> recentMessages = messageRepository.findMessageSince(timeAgo);
+        log.info("Got {} recent messages", recentMessages.size());
+        return recentMessages;
     }
     //get last N messages
     public List<Message> getLastMessages(int count){
-        return messageRepository.findLastMessages(count);
+        log.debug("Getting last ({}) messages", count);
+        List<Message> lastMessages = messageRepository.findLastMessages(count);
+        log.info("Got {} last messages", lastMessages.size());
+        return lastMessages;
     }
     //clear old messages (30d+)
     @Transactional
     public int clearOldMessages(){
+        log.debug("Clearing old ({}) messages", oldMessageDays);
         LocalDateTime timeAgo = LocalDateTime.now().minusDays(oldMessageDays);
-        return messageRepository.deleteOlderThan(timeAgo);
+        int clearedMessages = messageRepository.deleteOlderThan(timeAgo);
+        log.info("Cleared {} messages", clearedMessages);
+        return clearedMessages;
     }
 
 }
