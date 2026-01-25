1b873b44a7055fcee11498e3f367fe13f7641435
Lazy Fruit <117809440+Artificial-Fly@users.noreply.github.com>
2026-01-25 18:52:57 +0900

    add user service log

--- src/main/java/localchat/demo/service/UserService.java
+++ src/main/java/localchat/demo/service/UserService.java
@@ -3,6 +3,8 @@ package localchat.demo.service;
 import localchat.demo.entity.User;
 import localchat.demo.exception.UserNotFoundException;
 import localchat.demo.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -12,23 +14,32 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
 
+    private static Logger log = LoggerFactory.getLogger(UserService.class);
+
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+
+
     //Generate nickname
     private String generateNickname(){
+        log.debug("Generating nickname");
         String[] adjectives = {"Funny","Evil","Weird","Gloomy","Smart","Bright","Fast","Mysterious","Tall","Short"};
         String[] nouns = {"Bunny","Wolf","Racoon","Fox","Owl","Gazelle","Crow","Mice","Cat","Dog"};
         String randomAdj = adjectives[(int)(Math.random()*adjectives.length)];
         String randomNoun = nouns[(int)(Math.random()*nouns.length)];
-        return randomAdj+((int)(Math.random()*999))+randomNoun;
+        String randomNickname = randomAdj+((int)(Math.random()*999))+randomNoun;
+        log.info("Random nickaname {} generated", randomNickname);
+        return randomNickname;
     }
     //create user by mac adress
     public User CreateUserByUuid(String uuid){
+        log.debug("Creating new user or getting already existing");
         //check if user already exists
         Optional<User> existingUser = userRepository.findByUuid(uuid);
         if(existingUser.isPresent()){
+            log.debug("User {} ({}) exists",existingUser.get().getNickname(), existingUser.get().getId());
             return existingUser.get();
         }
         //generate unique nickname
@@ -42,17 +53,30 @@ public class UserService {
         user.setUuid(uuid);
         user.setCreatedAt(LocalDateTime.now());
         //save user
-        return userRepository.save(user);
+        User savedUser = userRepository.save(user);
+        log.info("User ({}, {}, {}) is saved", savedUser.getId(), savedUser.getUuid(), savedUser.getNickname());
+        return savedUser;
     }
     //get user by uuid
     public User getUserByUuid(String uuid){
-        return userRepository.findByUuid(uuid).orElseThrow(()-> new UserNotFoundException("User not found by uuid"));
+        log.debug("Getting user by uuid");
+        User getUser = userRepository.findByUuid(uuid).orElseThrow(()-> new UserNotFoundException("User not found by uuid"));
+        log.info("Got user {} ({}) by uuid {}", getUser.getId(), getUser.getNickname(), getUser.getUuid());
+        return getUser;
     }
     //get user by nickname
     public User getUserByNickname(String nickname){
-        return userRepository.findByNickname(nickname).orElseThrow(()-> new UserNotFoundException("User not found by nickname"));
+        log.debug("Getting user by nickname");
+        User getUser = userRepository.findByNickname(nickname).orElseThrow(()-> new UserNotFoundException("User not found by nickname"));
+        log.info("Got user {} ({}) by nickname {}", getUser.getId(), getUser.getUuid(), getUser.getNickname());
+        return getUser;
     }
 
-    public String getNicknameByUuid(String uuid){ return userRepository.findNicknameByUuid(uuid).orElse(null);}
+    public String getNicknameByUuid(String uuid){
+        log.debug("Getting Nickname by uuid");
+        String nickname = userRepository.findNicknameByUuid(uuid).orElse(null);
+        log.info("Got nickname {} by uuid {}", nickname, uuid);
+        return nickname;
+    }
 
 }
